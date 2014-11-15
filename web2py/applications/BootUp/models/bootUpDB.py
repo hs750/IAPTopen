9e49cf35e76bffb7639e5bb7aba98563b7969fd5
Harrison <hs750@york.ac.uk>
2014-11-15 11:24:59 +0000

    no longer user auth. reg and login and logout

--- web2py/applications/BootUp/models/bootUpDB.py
+++ web2py/applications/BootUp/models/bootUpDB.py
@@ -1,5 +1,4 @@
 __author__ = 'Y8191122'
-from gluon.tools import Auth
 
 db = DAL('sqlite://bootUpDB.db')
 
@@ -15,36 +14,31 @@ db.define_table('Addresses',
                 Field('StreetAddress', 'text', requires=IS_NOT_EMPTY()),
                 Field('City', 'string', requires=IS_NOT_EMPTY()),
                 Field('Country', 'string', requires=IS_NOT_EMPTY()),
-                Field('PostCode', 'string', requires=[IS_NOT_EMPTY(), IS_MATCH('[A-Z]{2}[1-9]{2} [1-9][A-Z]{2}',
-                                                              error_message='Not a valid Post Code')],
+                Field('PostCode', 'string', requires=[IS_NOT_EMPTY(), IS_MATCH('[A-Z]{2}[0-9]{2} [0-9][A-Z]{2}',
+                                                                               error_message='Not a valid Post Code')],
                       comment='Must match the form AB01 2CD')
                 )
 
 db.define_table('CreditCards',
                 Field('CardNumber', 'decimal(12,0)', requires=[IS_NOT_EMPTY(), IS_DECIMAL_IN_RANGE(0, 1e100)]),
                 Field('ExpiryDate', 'date',
-                      requires=[IS_NOT_EMPTY(), IS_DATE(format=T('%Y-%m-%d'), error_message='must be YYYY-MM-DD!')]),
+                      requires=[IS_NOT_EMPTY(), IS_DATE(format='%d-%m-%Y', error_message='Must be DD-MM-YYYY!')]),
                 Field('IDCode', 'decimal(3,0)', requires=[IS_NOT_EMPTY(), IS_DECIMAL_IN_RANGE(0, 1e100)]),
                 Field('addressID', db.Addresses, requires=IS_NOT_EMPTY())
                 )
 
-"""
-    The Users table is not explicitly defined. Instead I use the authentication library built into web2py with
-    some fields added.
-"""
-auth = Auth(db)
-"""
-    Custom fields added to the web2py authentication.
-    Fields already provided by web2py authentication:
-        username, email, name (split into first and last)
-"""
-auth.settings.extra_fields['auth_user'] = [
-    Field('DateOfBirth', 'date',
-          requires=[IS_NOT_EMPTY(), IS_DATE(format=T('%Y-%m-%d'), error_message='must be YYYY-MM-DD!')]),
-    Field('addressID', db.Addresses, requires=IS_NOT_EMPTY()),
-    Field('cardID', db.CreditCards, requires=IS_NOT_EMPTY())
-]
-auth.define_tables(username=True) #Allows login using username
+db.define_table('Users',
+                Field('FirstName', 'string', requires=IS_NOT_EMPTY()),
+                Field('LastName', 'string', requires=IS_NOT_EMPTY()),
+                Field('Email', 'string', requires=IS_EMAIL()),
+                Field('Username', 'string', requires=[IS_NOT_EMPTY(),
+                                                      IS_NOT_IN_DB(db, 'Users.Username', '%(Username)s')],
+                      unique=True),
+                Field('Password', 'string', requires=IS_NOT_EMPTY()),
+                Field('DateOfBirth', 'date',
+                      requires=[IS_NOT_EMPTY(), IS_DATE(format='%d-%m-%Y', error_message='Must be DD-MM_YYYY!')]),
+                Field('addressID', db.Addresses, requires=IS_NOT_EMPTY()),
+                Field('cardID', db.CreditCards, requires=IS_NOT_EMPTY()))
 
 db.define_table('Bootables',
                 Field('Title', 'string', requires=IS_NOT_EMPTY()),
@@ -60,7 +54,7 @@ db.define_table('Bootables',
                 Field('PersonalStory', 'text', requires=IS_NOT_EMPTY()),
                 Field('State', 'string',
                       requires=IS_IN_SET(['Not Available', 'Open for Pledges', 'Funded', 'Not Funded'])),
-                Field('userID', db.auth_user, requires=IS_NOT_EMPTY())
+                Field('userID', db.Users, requires=IS_NOT_EMPTY())
                 )
 
 pledge_value_type = 'decimal(8,2)'  # maximum value of 999,999.99
@@ -81,7 +75,7 @@ db.define_table('PledgeRewards',
                 )
 
 db.define_table('UserPledges',
-                Field('userID', db.auth_user, requires=IS_NOT_EMPTY()),
+                Field('userID', db.Users, requires=IS_NOT_EMPTY()),
                 Field('bootID', db.Bootables, requires=IS_NOT_EMPTY()),
                 Field('Value', pledge_value_type, requires=IS_IN_DB(db, 'Pledges.Value', '%(Value)s')),
                 primarykey=['userID', 'bootID']
