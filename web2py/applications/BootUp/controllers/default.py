01dda79e30dc58759fe4bc32a60b237b659ad9a2
Harrison <hs750@york.ac.uk>
2014-11-18 11:47:57 +0000

    pass newest and top 5 to view for home page

--- web2py/applications/BootUp/controllers/default.py
+++ web2py/applications/BootUp/controllers/default.py
@@ -2,7 +2,9 @@ __author__ = "Y8191122"
 
 
 def index():
-    return dict()
+    newest = db(db.Bootables.id>0).select(orderby=~db.Bootables.id, limitby=(0, 5))
+    top5 = getTop5()
+    return dict(newest=newest, top=top5)
 
 
 def user():
