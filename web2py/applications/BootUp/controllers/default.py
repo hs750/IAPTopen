649af99535d0d819e017d5166c72ea0c5235e04a
Harrison <hs750@york.ac.uk>
2014-11-19 22:44:35 +0000

    moved view bootable to default

--- web2py/applications/BootUp/controllers/default.py
+++ web2py/applications/BootUp/controllers/default.py
@@ -7,4 +7,47 @@ def index():
     return dict(newest=newest, top=top5)
 
 
+def view():
+    """
+    View a particular bootable,
+    which bootable is determined by request.args(0) which is the bootID
+    :return: booable information, the percentage complete and the users who pledged with their pledge amount
+    """
+    bootID = request.args(0)
+    bootable = db(db.Bootables.id == bootID).select()
+
+    total = getTotalPledged(bootID)
+    completion = getCompletionPercentage(bootID)
+
+    pledgeValues = db(db.Pledges.bootID == bootID).select('Value')
+    values = []
+    for item in pledgeValues:
+        values += [item.Value]
+
+    print(values)
+    pledgeForm = FORM(SELECT(values, _name='pledgeValue'),
+                      INPUT(_type='submit', _name='pledgeSubmit', _value='Pledge!'),
+                      _name='pledgeForm',
+                      _class='form-inline')
+
+    if session.user is not None:
+        #Only do anything with the form if user logged in
+        if pledgeForm.accepts(request.post_vars, session):
+            db.UserPledges.insert(userID=session.user,
+                                  bootID=bootID,
+                                  Value=request.post_vars.pledgeValue)
+            response.flash = 'You have successfully pledged!'
+        elif pledgeForm.errors:
+            response.flash = 'There was a problem with your pledge'
+    else:
+        #Should never get here as form shouldnt be displayed if no user signed in
+        response.flash = 'Must be signed in to pledge!'
+
+    #Get this after submition of form so that screen updated on refresh
+    usersPledged = db((db.Bootables.id == bootID) &
+                      (db.Bootables.id == db.UserPledges.bootID) &
+                      (db.UserPledges.userID == db.Users.id)).select('Users.Username', 'UserPledges.Value')
+
+    return dict(bootable=bootable, total=total, percent=completion, users=usersPledged, pledgeForm=pledgeForm)
+
 
