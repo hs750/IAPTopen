03c0a627eb400975316f9a244a0e396c7841ab77
Harrison <hs750@york.ac.uk>
2014-11-20 09:39:07 +0000

    misc

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b07453ff632df1e5$cce17ddcb9c461fe6f1d27eb3cffb32d5a6a3357"
+password="pbkdf2(1000,20,sha512)$b0a1b85922336c86$63880920fb02a3343bfcfdf01b01c960f9c9a8d7"
