0aa6a0e05ee7a324c4211afcfa2247aaa106e1e5
Harrison <hs750@york.ac.uk>
2014-11-22 13:12:18 +0000

    display paragraphs correctly

--- web2py/applications/BootUp/models/a_constants.py
+++ web2py/applications/BootUp/models/a_constants.py
@@ -4,3 +4,14 @@ __author__ = 'Y8191122'
 #These variables are intended as global constants
 bootableCategories = ['Art', 'Comics', 'Crafts', 'Fashion', 'Film', 'Games', 'Music', 'Photography', 'Technology']
 bootableStates = ['Not Available', 'Open for Pledges', 'Funded', 'Not Funded']
+
+
+#This is intended to be a method available anywhere
+def paragraphise(text):
+    """
+    Takes text and replaces newlines in it with <br> so that paragraphs get displayed correctly
+    :param text: input text
+    :return: xml of input text
+    """
+    text = text.replace('\n', '<br>')
+    return XML(text)
